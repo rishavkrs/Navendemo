f5bf36d1517205e13e86c218e9693df47dc4f622
rishavkrs <rishavk576@gmail.com>
2023-06-23 10:26:20 +0000

    second

---
+++ Demo2/src/main/java/com/acc/lkm/App.java
@@ -0,0 +1,13 @@
+package com.acc.lkm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
